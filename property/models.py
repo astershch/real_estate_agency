8c53f5d88d88c4f5357281f9b5d5c8f88303ba1f
astershch <107745329+astershch@users.noreply.github.com>
2022-12-18 14:56:04 +0600

    remove unused fields in db model Flat

--- property/models.py
+++ property/models.py
@@ -5,16 +5,6 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Flat(models.Model):
-    owner = models.CharField('ФИО владельца', max_length=200)
-
-    owner_pure_phone = PhoneNumberField(
-        'Нормализованный номер владельца',
-        blank=True,
-        null=True
-    )
-
-    owners_phonenumber = models.CharField('Номер владельца', max_length=20)
-
     new_building = models.BooleanField(blank=True, null=True)
 
     description = models.TextField('Текст объявления', blank=True)
